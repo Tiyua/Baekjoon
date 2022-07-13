5515c78da9c6ddb64a44cac2ada95ee32a337255
Tiyua <101155063+Tiyua@users.noreply.github.com>
2022-07-13 23:06:56 +0900

    Create 백준단계별1.py

---
+++ 백준단계별1.py
@@ -0,0 +1,65 @@
+# 2557
+print("Hello World!")
+
+# 10718
+for _ in range(2):
+    print("강한친구 대한육군")
+
+# 10171
+print("\    /\ \n )  ( ')\n(  /  )\n \(__)|")
+
+# 10172
+print("|\_/|\n|q p|   /}\n( 0 )\"\"\"\ \n|\"^\"`    |\n||_/=\\\\__|")
+
+# 1000
+a, b = map(int, input().split())
+print(a+b)
+
+# 1001
+a, b = map(int, input().split())
+print(a-b)
+
+# 10998
+a, b = map(int, input().split())
+print(a*b)
+
+# 1008
+a, b = map(int, input().split())
+print(a/b)
+
+# 10869
+a, b = map(int, input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+
+# 10926
+a = input()
+print(a+"??!")
+
+# 18108
+a = int(input())
+print(a-543)
+
+# 10430
+a, b, c = map(int, input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+
+# 2588
+a = int(input())
+b = int(input())
+c = b//100
+d = b//10 - c*10
+f = b - c*100 - d*10
+print(a*f)
+print(a*d)
+print(a*c)
+print(a*b)
+
+# 25083
+print("         ,r'\"7\nr`-_   ,'  ,/\n \. \". L_r'\n   `~\/\n      |\n      |")
